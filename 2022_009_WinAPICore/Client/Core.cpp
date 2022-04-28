a3c556944a09abdd0ed09ced13ee2f4170e38190
Jay9011 <whdtjq5904@gmail.com>
2022-04-28 19:13:43 +0900

    윈도우 크기 변경 (AdjustWindowRect, SetWindowPos)

--- 2022_009_WinAPICore/Client/Core.cpp
+++ 2022_009_WinAPICore/Client/Core.cpp
@@ -2,11 +2,15 @@
 #include "Core.h"
 
 Core::Core()
+	: m_hWnd(0)
+	, m_ptResolution{}
+	, m_hdc(0)
 {
 }
 
 Core::~Core()
 {
+	ReleaseDC(m_hWnd, m_hdc);
 }
 
 int Core::init(HWND _hWnd, POINT _ptResolution)
@@ -15,10 +19,17 @@ int Core::init(HWND _hWnd, POINT _ptResolution)
 	m_ptResolution = _ptResolution;
 
 	// 해상도에 맞게 윈도우 크기 조정
+	RECT rt = { 0, 0, m_ptResolution.x, m_ptResolution.y };
+	AdjustWindowRect(&rt, WS_OVERLAPPEDWINDOW, true);
+	SetWindowPos(m_hWnd, nullptr, 100, 100, rt.right - rt.left, rt.bottom - rt.top, 0);
+
+	m_hdc = GetDC(m_hWnd);
 
 	return S_OK;
 }
 
 void Core::progress()
 {
+	// 그리기
+	Rectangle(m_hdc, 10, 10, 110, 110);
 }
--- 2022_009_WinAPICore/Client/Core.h
+++ 2022_009_WinAPICore/Client/Core.h
@@ -10,6 +10,7 @@ private:
 private:
 	HWND	m_hWnd;			// 메인 윈도우 핸들
 	POINT	m_ptResolution;	// 메인 윈도우 해상도
+	HDC		m_hdc;			// 메인 윈도우에 Draw 할 DC
 
 public:
 	int init(HWND _hWnd, POINT _ptResolution);
